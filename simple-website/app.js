8899325d7e4f882e194f90de39b7a965b5d9ad09
Aneetta Sara <39122867+aneettasara@users.noreply.github.com>
2021-03-30 01:03:06 +0530

    Add files via upload

---
+++ simple-website/app.js
@@ -0,0 +1,19 @@
+'use strict'
+alert('A Website using HTML, CSS, and JavaScript')
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+    
+    console.log('current class name: ' + className);
+});
+
